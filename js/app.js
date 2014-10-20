fec2b515f3f1746f0a23ecc84cc16d98f229565e
Beena Avireni <beena@avireni.com>
2014-10-20 00:48:08 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,22 +4,19 @@
 
 
 (function () {
-  var app = angular.module('gemStore', ['store-directives']);
-
-  app.controller('StoreController', ['$http',
+  var app = angular.module('gemStore', ['store-directives']); // give gemStore module access to the directives by adding a dependency to its definition.
+    app.controller('StoreController', ['$http',              // built in $http Service to make requests to a server(or in our case a json file).
     function ($http) {
       var store = this;
       store.products = [];
-      $http.get('store-products.json').success(function (data) {
+      $http.get('store-products.json').success(function (data) {  //Give our StoreController access to the products by setting products equal to the data given to us with the http service's success promise.
         store.products = data;
       });
    }]);
   app.controller('ReviewController', function () {
     this.review = {};
-
     this.addReview = function (product) {
       product.reviews.push(this.review);
-
       this.review = {};
     };
   });
